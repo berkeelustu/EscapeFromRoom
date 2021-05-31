c60b2e887a2ade39c6f53df14ef1466575590efe
Berke Elüstü <77470163+berkeelustu@users.noreply.github.com>
2021-06-01 01:58:11 +0300

    Update OpenDoor.h

--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -14,12 +12,12 @@ class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
+	
 	UOpenDoor();
 	
 
 protected:
-	// Called when the game starts
+	
 	virtual void BeginPlay() override;
 
 public:	
