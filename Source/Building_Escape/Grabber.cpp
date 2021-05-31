27fe2d8e75d8cc77d889dc77ddc488b7bb9af06a
Berke Elüstü <77470163+berkeelustu@users.noreply.github.com>
2021-06-01 01:56:04 +0300

    Update Grabber.cpp

--- Source/Building_Escape/Grabber.cpp
+++ Source/Building_Escape/Grabber.cpp
@@ -7,19 +7,15 @@
 #define OUT
 
 
-// Sets default values for this component's properties
 UGrabber::UGrabber()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 }
-// Called when the game starts
+
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-// Checks for Physics Handle Component
     FindPhysicsHandle ();
 	SetUpInputComponent ();
 }
@@ -55,7 +51,7 @@ void UGrabber::Release()
 	UE_LOG(LogTemp, Warning, TEXT("I release the item."));
 }
 
-// Called every frame
+
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
