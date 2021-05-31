e05c08f034a52dc816d9489530834262616c53d2
Berke Elüstü <77470163+berkeelustu@users.noreply.github.com>
2021-06-01 01:58:35 +0300

    Update World_Position.cpp

--- Source/Building_Escape/World_Position.cpp
+++ Source/Building_Escape/World_Position.cpp
@@ -1,22 +1,14 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "World_Position.h"
 #include "GameFramework/Actor.h"
 
 
-// Sets default values for this component's properties
 UWorld_Position::UWorld_Position()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
-// Called when the game starts
+
 void UWorld_Position::BeginPlay()
 {
 	Super::BeginPlay();
@@ -29,8 +21,6 @@ void UWorld_Position::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("%s's Position is: %s"), *ObjectName, *ObjectPosition);
 }
 
-
-// Called every frame
 void UWorld_Position::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
